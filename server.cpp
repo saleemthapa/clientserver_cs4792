8ff75d6f4e20659e75e11e0a1b2147f6886c9c6d
Saleem Thapa <99854920+saleemthapa@users.noreply.github.com>
2024-02-10 12:53:01 -0600

    Update server.cpp
    added functionality to create save folder and accept the mentioned port

--- server.cpp
+++ server.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <fstream>
 #include <unistd.h>
+#include <cstring>
+#include <cstdlib> // For exit()
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
 
 void error(const char *msg) {
@@ -8,7 +12,14 @@ void error(const char *msg) {
     exit(1);
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " port_number" << std::endl;
+        return -1;
+    }
+
+    int serverPort = atoi(argv[1]);
+
     // Create a socket
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
     if (serverSocket == -1) {
@@ -19,7 +30,7 @@ int main() {
     // Bind the socket to an IP address and port
     sockaddr_in serverAddress;
     serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(8080);  // You can choose any available port
+    serverAddress.sin_port = htons(serverPort);
     serverAddress.sin_addr.s_addr = INADDR_ANY;
 
     if (bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) {
@@ -35,7 +46,7 @@ int main() {
         return -1;
     }
 
-    std::cout << "Server listening on port 8080..." << std::endl;
+    std::cout << "Server listening on port " << serverPort << "..." << std::endl;
 
     // Accept a connection from a client
     sockaddr_in clientAddress;
@@ -49,8 +60,31 @@ int main() {
 
     std::cout << "Connection accepted from " << inet_ntoa(clientAddress.sin_addr) << std::endl;
 
-    // Receive and save data from the client into a file
-    std::ofstream outputFile("received_data.txt", std::ios::out | std::ios::binary);
+    // Receive filename and data size from the client
+    char filename[256];
+    size_t dataSize;
+    ssize_t bytesRead = recv(clientSocket, &dataSize, sizeof(dataSize), 0);
+    if (bytesRead != sizeof(dataSize)) {
+        std::cerr << "Error receiving data size" << std::endl;
+        close(clientSocket);
+        close(serverSocket);
+        return -1;
+    }
+    dataSize = ntohl(dataSize); // Convert from network byte order to host byte order
+
+    bytesRead = recv(clientSocket, filename, sizeof(filename), 0);
+    if (bytesRead <= 0) {
+        std::cerr << "Error receiving filename" << std::endl;
+        close(clientSocket);
+        close(serverSocket);
+        return -1;
+    }
+
+    // Create folder if not exists
+    system("mkdir -p /save/");
+
+    // Open file in save folder
+    std::ofstream outputFile("/save/" + std::string(filename), std::ios::out | std::ios::binary);
     if (!outputFile.is_open()) {
         std::cerr << "Error opening output file" << std::endl;
         close(clientSocket);
@@ -59,8 +93,17 @@ int main() {
     }
 
     char buffer[1024];
-    ssize_t bytesRead;
-    while ((bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0)) > 0) {
+    size_t totalBytesReceived = 0;
+    while (totalBytesReceived < dataSize) {
+        bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
+        if (bytesRead <= 0) {
+            std::cerr << "Error receiving data from client" << std::endl;
+            close(clientSocket);
+            close(serverSocket);
+            outputFile.close();
+            return -1;
+        }
+        totalBytesReceived += bytesRead;
         outputFile.write(buffer, bytesRead);
     }
 
@@ -69,8 +112,7 @@ int main() {
     close(serverSocket);
     outputFile.close();
 
-    std::cout << "Data received and saved to 'received_data.txt'" << std::endl;
+    std::cout << "Data received and saved to '/save/" << filename << "'" << std::endl;
 
     return 0;
 }
-
